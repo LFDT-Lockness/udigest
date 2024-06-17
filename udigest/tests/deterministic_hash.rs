36f9d34fd556407c2092eea360aeeafd25e7d494
Denis Varlakov <denis@dfns.co>
2024-06-17 16:22:15 +0200

    Fix clippy warns in tests

--- udigest/tests/deterministic_hash.rs
+++ udigest/tests/deterministic_hash.rs
@@ -40,7 +40,7 @@ fn shake256() {
     let mut alice_hash_reader = udigest::hash_xof::<sha3::Shake256>(&ALICE);
     alice_hash_reader.read(&mut hash);
     assert_eq!(
-        hex::encode(&hash),
+        hex::encode(hash),
         "ee629bcc426422887fe6f9a9a3384128bd5efc3c623a4599c8526c24a97972be\
         2a325ef03c95ac649b77f0193c901c942762e93fd939372ef484681220c6fc0b\
         0dc12be8c6b9ee914dac34697d0deeb3a3e510f24a1b0bfc24d144b639a66c6a\
@@ -50,7 +50,7 @@ fn shake256() {
     let mut bob_hash_reader = udigest::hash_xof::<sha3::Shake256>(&BOB);
     bob_hash_reader.read(&mut hash);
     assert_eq!(
-        hex::encode(&hash),
+        hex::encode(hash),
         "56cd71e796fc94176923b73bfe3f659ea7a9a666a2faae6020d1c4f41a51035a\
         e7965583087f1badf452a40036499d54075350d8e64e5b68b0f3f52c286c15e3\
         cb010249754a0c7f263d14c7a284da134ca133df84c62d80adfdb0ec0d5c3f0a\
@@ -64,14 +64,14 @@ fn blake2b() {
 
     udigest::hash_vof::<blake2::Blake2bVar>(&ALICE, &mut out).unwrap();
     assert_eq!(
-        hex::encode(&out),
+        hex::encode(out),
         "57b2a8a078ca3b04dc72b308696bc4715c62593b461608bff01388ef3bd49fed\
         244bd2e9407965ec2bfe13781ae3cd28ea0cb08fb4b46824ea7909c488fec8"
     );
 
     udigest::hash_vof::<blake2::Blake2bVar>(&BOB, &mut out).unwrap();
     assert_eq!(
-        hex::encode(&out),
+        hex::encode(out),
         "83aa6240d105ec1b496e6963dbab3e48fd09860b734c963b59ee764781d922f1\
         207405232c1d84965b32f6a73b182b224d1533859f586c332377fe4a39489e"
     );
