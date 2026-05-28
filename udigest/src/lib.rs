cbb5ec093bbf594925bace31ee96a3e45041604c
Denis Varlakov <denis@dfns.co>
2026-05-28 15:46:05 +0200

    Update Discord link to LFDT-wide invite
    Signed-off-by: Denis Varlakov <denis@dfns.co>

--- udigest/src/lib.rs
+++ udigest/src/lib.rs
@@ -1,7 +1,7 @@
 //! ![License](https://img.shields.io/crates/l/udigest.svg)
 //! [![Docs](https://docs.rs/udigest/badge.svg)](https://docs.rs/udigest)
 //! [![Crates io](https://img.shields.io/crates/v/udigest.svg)](https://crates.io/crates/udigest)
-//! [![Discord](https://img.shields.io/discord/905194001349627914?logo=discord&logoColor=ffffff&label=Discord)](https://discordapp.com/channels/905194001349627914/1285268686147424388)
+//! [![Discord](https://img.shields.io/discord/905194001349627914?logo=discord&logoColor=ffffff&label=Discord)](https://discord.com/invite/hyperledger)
 //! [![OpenSSF Scorecard](https://api.scorecard.dev/projects/github.com/LFDT-Lockness/udigest/badge)](https://scorecard.dev/viewer/?uri=github.com/LFDT-Lockness/udigest)
 //!
 //! # Unambiguously digest structured data
@@ -51,7 +51,7 @@
 //! * `derive` enables `Digestable` proc macro
 //!
 //! ## Join us in Discord!
-//! Feel free to reach out to us [in Discord](https://discordapp.com/channels/905194001349627914/1285268686147424388)!
+//! Feel free to reach out to us [in Discord](https://discord.com/invite/hyperledger)!
 
 #![no_std]
 #![forbid(missing_docs)]
